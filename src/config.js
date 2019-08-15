c85440ece86621ee10926d77379e3a68a2f00715
robdy <robdyjas@gmail.com>
2019-08-15 21:10:16 +0200

    Adds config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports =  {
+  "token": process.env.DISCORDTOKEN,
+  "databaseURL": process.env.DBURL,
+  "reaction_timeout": "30000",
+  "pager_timeout": "60000",
+  "prefix": "$",
+  "owner_id": process.env.OWNERID
+}
